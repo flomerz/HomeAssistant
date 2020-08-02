0da4db88ba838e29fc9d5d7bc3796904d7562901
Florian Merz <florian.p.m@gmail.com>
2020-08-02 16:58:49 +0200

    new version

--- custom_components/shelly/config_flow.py
+++ custom_components/shelly/config_flow.py
@@ -1,4 +1,4 @@
-"""Adds config flow for Shelly."""
+"""Adds config flow for HACS."""
 # pylint: disable=dangerous-default-value
 import logging
 import voluptuous as vol
@@ -62,10 +62,10 @@ class ShellyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         return ShellyOptionsFlowHandler(config_entry)
 
 class ShellyOptionsFlowHandler(config_entries.OptionsFlow):
-    """Shelly config flow options handler."""
+    """HACS config flow options handler."""
 
     def __init__(self, config_entry):
-        """Initialize Shelly options flow."""
+        """Initialize HACS options flow."""
         self.config_entry = config_entry
         self._options = {}
         self._step_cnt = 0
