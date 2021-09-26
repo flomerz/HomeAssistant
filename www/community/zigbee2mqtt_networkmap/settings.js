8799bbdae519da1ed7928cfe57229ee6b7fd006c
Florian Merz <florian.p.m@gmail.com>
2021-09-26 15:00:15 +0200

    fix not home

--- www/community/zigbee2mqtt_networkmap/settings.js
+++ www/community/zigbee2mqtt_networkmap/settings.js
@@ -1,2 +1,2 @@
-var webhook_trigger_update_id = 'ea534004b92c8d090f6048a4b5d3fa7d19eb7c8c1a27fcd131964be37c7f5b43';
-var webhook_check_update_id = '9a8a307953c7915cd38e78a2ef05c873f80863d2581fb4a4fe6c3b3f7adaf78c';
+var webhook_trigger_update_id = '787c5cc10b766225cb510646ed84a471dfa658a077db800f59a7eb60fa19da5b';
+var webhook_check_update_id = 'a22e28fa7ea10580499cdf8d061b569870a21611de8bd2fe6dee66cd9e9ac1b6';
