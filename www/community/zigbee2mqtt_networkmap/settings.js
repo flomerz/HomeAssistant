7dbbb5ea271a062322bf78a18bd31602559aef34
Florian Merz <florian.p.m@gmail.com>
2021-10-08 14:10:01 +0200

    update

--- www/community/zigbee2mqtt_networkmap/settings.js
+++ www/community/zigbee2mqtt_networkmap/settings.js
@@ -1,2 +1,2 @@
-var webhook_trigger_update_id = '787c5cc10b766225cb510646ed84a471dfa658a077db800f59a7eb60fa19da5b';
-var webhook_check_update_id = 'a22e28fa7ea10580499cdf8d061b569870a21611de8bd2fe6dee66cd9e9ac1b6';
+var webhook_trigger_update_id = '60d88b3a92e4c14a01d8596856ee0a5f5fd2fe8e1467fbd23b5dbc8b0a4394ed';
+var webhook_check_update_id = 'cad6f4d2cc8eafadc4fcbbcae7e7be5d4a7915104a44146af79b11e8be8b3d5b';
